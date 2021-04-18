d4879037b9ebf2f329152cf9517c1c97fde7d844
beth-2 <77358378+beth-2@users.noreply.github.com>
2021-04-18 16:49:11 +0300

    First React App

---
+++ src/Components/Bind.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Bind extends Component {
+    render() {
+        return (
+            <div>
+                <h1>My First React Project</h1>
+            </div>
+        )
+    }
+}
